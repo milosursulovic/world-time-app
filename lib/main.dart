98e3c4c3a21e425ea7cabb7afde9360a67216505
Misa <milosursulovic2696@gmail.com>
2023-01-04 22:20:25 +0100

    Add different pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:world_time_app/pages/home.dart';
 
 void main() {
-  runApp(const Scaffold());
+  runApp(const MaterialApp(
+    home: Home(),
+  ));
 }
