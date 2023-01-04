8b75ffe4fe8c596eb7072e1e78bc4fc20556e751
Misa <milosursulovic2696@gmail.com>
2023-01-04 23:26:54 +0100

    Get data from world time api

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 
 class Loading extends StatefulWidget {
   const Loading({Key? key}) : super(key: key);
@@ -8,6 +11,22 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
+  void getTime() async {
+    Response response = await get(
+        Uri.parse("https://worldtimeapi.org/api/timezone/Europe/London"));
+    Map data = jsonDecode(response.body);
+    String datetime = data['datetime'];
+    String offset = data['utc_offset'].substring(1, 3);
+    DateTime now = DateTime.parse(datetime);
+    now = now.add(Duration(hours: int.parse(offset)));
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getTime();
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
