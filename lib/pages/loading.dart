57e7c8bcef25123cdfd33538f3a09d049dd7ac00
Misa <milosursulovic2696@gmail.com>
2023-01-05 13:34:55 +0100

    Add spinner on loading screen

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:world_time_app/services/world_time.dart';
 
 class Loading extends StatefulWidget {
@@ -27,10 +28,13 @@ class _LoadingState extends State<Loading> {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Padding(
-        padding: EdgeInsets.all(50.0),
-        child: Text("Loading"),
+    return Scaffold(
+      backgroundColor: Colors.blue[900],
+      body: const Center(
+        child: SpinKitFadingCube(
+          color: Colors.white,
+          size: 50.0,
+        ),
       ),
     );
   }
