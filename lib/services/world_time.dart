9e5346329c5927e6ca28f6dd8659f9a5ab48b9b9
Misa <milosursulovic2696@gmail.com>
2023-01-05 12:21:24 +0100

    Add error handling for network call

--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -11,14 +11,19 @@ class WorldTime {
   WorldTime(this._location, this._flag, this._url);
 
   Future<void> getTime() async {
-    Response response =
-        await get(Uri.parse("https://worldtimeapi.org/api/timezone/$_url"));
-    Map data = jsonDecode(response.body);
-    String datetime = data['datetime'];
-    String offset = data['utc_offset'].substring(1, 3);
-    DateTime now = DateTime.parse(datetime);
-    now = now.add(Duration(hours: int.parse(offset)));
-    _time = now.toString();
+    try {
+      Response response =
+          await get(Uri.parse("https://worldtimeapi.org/api/timezone/$_url"));
+      Map data = jsonDecode(response.body);
+      String datetime = data['datetime'];
+      String offset = data['utc_offset'].substring(1, 3);
+      DateTime now = DateTime.parse(datetime);
+      now = now.add(Duration(hours: int.parse(offset)));
+      _time = now.toString();
+    } catch (e) {
+      print("Caught error: $e");
+      _time = "Could not get time data";
+    }
   }
 
   String get time => _time;
