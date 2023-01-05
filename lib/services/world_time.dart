66b2c522382430c367f27f70da373d65c175674f
Misa <milosursulovic2696@gmail.com>
2023-01-05 14:54:41 +0100

    Add daytime mode

--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -8,21 +8,25 @@ class WorldTime {
   late String _time;
   final String _flag;
   final String _url;
+  late bool _isDaytime;
 
   WorldTime(this._location, this._flag, this._url);
 
   Future<void> getTime() async {
     try {
       Response response =
-          await get(Uri.parse("https://worldtimeapi.org/api/timezone/$_url"));
+          await get(Uri.parse("http://worldtimeapi.org/api/timezone/$_url"));
       Map data = jsonDecode(response.body);
       String datetime = data["datetime"];
       String offset = data["utc_offset"].substring(1, 3);
       DateTime now = DateTime.parse(datetime);
       now = now.add(Duration(hours: int.parse(offset)));
+      _isDaytime = now.hour > 6 && now.hour < 20;
       _time = DateFormat.jm().format(now);
     } catch (e) {
+      print(e);
       _time = "Could not get time data";
+      _isDaytime = false;
     }
   }
 
@@ -31,4 +35,6 @@ class WorldTime {
   String get location => _location;
 
   String get flag => _flag;
+
+  bool get isDaytime => _isDaytime;
 }
